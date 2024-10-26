5b85ac0762aff3897764037e4547a035e3fbed37
Caio Pereira <caiopereira1998@hotmail.com>
2024-10-26 19:41:14 -0300

    Rolagem adicionada a pagina

--- lib/screen/home_page_screen.dart
+++ lib/screen/home_page_screen.dart
@@ -28,108 +28,110 @@ class _HomePageScreenState extends State<HomePageScreen> {
         title: const Text('Sorteio de Números'),
         backgroundColor: Colors.blueGrey,
       ),
-      body: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            const Text(
-              'Configure o sorteio',
-              style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
-            ),
-            const SizedBox(height: 20),
-            TextField(
-              controller: _quantityController,
-              keyboardType: TextInputType.number,
-              inputFormatters: [FilteringTextInputFormatter.digitsOnly,],
-              decoration: CustomInputDecoration.build(
-                labelText: 'Quantos números serão sorteados?',
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              const Text(
+                'Configure o sorteio',
+                style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
               ),
-            ),
-            const SizedBox(height: 10),
-            Row(
-              children: [
-                Expanded(
+              const SizedBox(height: 20),
+              TextField(
+                controller: _quantityController,
+                keyboardType: TextInputType.number,
+                inputFormatters: [FilteringTextInputFormatter.digitsOnly,],
+                decoration: CustomInputDecoration.build(
+                  labelText: 'Quantos números serão sorteados?',
+                ),
+              ),
+              const SizedBox(height: 10),
+              Row(
+                children: [
+                  Expanded(
+                      child: TextField(
+                        controller: _minController,
+                        keyboardType: TextInputType.number,
+                        inputFormatters: [FilteringTextInputFormatter.digitsOnly,],
+                        decoration: CustomInputDecoration.build(
+                          labelText: 'Valor mínimo',
+                        ),
+                      ),
+                  ),
+                  const SizedBox(width: 10),
+                  Expanded(
                     child: TextField(
-                      controller: _minController,
+                      controller: _maxController,
                       keyboardType: TextInputType.number,
                       inputFormatters: [FilteringTextInputFormatter.digitsOnly,],
                       decoration: CustomInputDecoration.build(
-                        labelText: 'Valor mínimo',
+                        labelText: 'Valor máximo',
                       ),
                     ),
-                ),
-                const SizedBox(width: 10),
-                Expanded(
-                  child: TextField(
-                    controller: _maxController,
-                    keyboardType: TextInputType.number,
-                    inputFormatters: [FilteringTextInputFormatter.digitsOnly,],
-                    decoration: CustomInputDecoration.build(
-                      labelText: 'Valor máximo',
-                    ),
-                  ),
-                )
-              ],
-            ),
-            const SizedBox(height: 20),
-            ElevatedButton(
-              style: ElevatedButton.styleFrom(
-                padding: const EdgeInsets.symmetric(horizontal: 50, vertical: 15),
-                textStyle: const TextStyle(fontSize: 18),
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(12),
-                ),
+                  )
+                ],
               ),
-              onPressed: () {
-                _sortearNumeros();
-                customSnackBar(
-                  context,
-                  'Foram sorteados ${_sortedNumbers.length} números!',
-                  backgroundColor: Colors.green,
-                );
-              },
-              child: const Text('Sortear'),
-            ),
-            const SizedBox(height: 20),
-            _sortedNumbers.isNotEmpty
-                ? Column(
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: [
-                const Text(
-                  'Números Sorteados:',
-                  style: TextStyle(
-                      fontSize: 18, fontWeight: FontWeight.bold),
-                ),
-                const SizedBox(height: 10),
-                Container(
-                  padding: const EdgeInsets.all(16.0),
-                  decoration: BoxDecoration(
-                    color: Colors.teal[50],
+              const SizedBox(height: 20),
+              ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                  padding: const EdgeInsets.symmetric(horizontal: 50, vertical: 15),
+                  textStyle: const TextStyle(fontSize: 18),
+                  shape: RoundedRectangleBorder(
                     borderRadius: BorderRadius.circular(12),
-                    border: Border.all(color: Colors.blueGrey, width: 2),
-                  ),
-                  child: Wrap(
-                    spacing: 10,
-                    runSpacing: 10,
-                    children: _sortedNumbers
-                        .map((num) => Chip(
-                      label: Text(
-                        num.toString(),
-                        style: const TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
-                      ),
-                      backgroundColor: Colors.teal[100],
-                    ))
-                        .toList(),
                   ),
                 ),
-              ],
-            )
-                : const Text(
-              'Nenhum número sorteado ainda',
-              style: TextStyle(fontSize: 16),
-            ),
-          ],
+                onPressed: () {
+                  _sortearNumeros();
+                  customSnackBar(
+                    context,
+                    'Foram sorteados ${_sortedNumbers.length} números!',
+                    backgroundColor: Colors.green,
+                  );
+                },
+                child: const Text('Sortear'),
+              ),
+              const SizedBox(height: 20),
+              _sortedNumbers.isNotEmpty
+                  ? Column(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  const Text(
+                    'Números Sorteados:',
+                    style: TextStyle(
+                        fontSize: 18, fontWeight: FontWeight.bold),
+                  ),
+                  const SizedBox(height: 10),
+                  Container(
+                    padding: const EdgeInsets.all(16.0),
+                    decoration: BoxDecoration(
+                      color: Colors.teal[50],
+                      borderRadius: BorderRadius.circular(12),
+                      border: Border.all(color: Colors.blueGrey, width: 2),
+                    ),
+                    child: Wrap(
+                      spacing: 10,
+                      runSpacing: 10,
+                      children: _sortedNumbers
+                          .map((num) => Chip(
+                        label: Text(
+                          num.toString(),
+                          style: const TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+                        ),
+                        backgroundColor: Colors.teal[100],
+                      ))
+                          .toList(),
+                    ),
+                  ),
+                ],
+              )
+                  : const Text(
+                'Nenhum número sorteado ainda',
+                style: TextStyle(fontSize: 16),
+              ),
+            ],
+          ),
         ),
       ),
     );
